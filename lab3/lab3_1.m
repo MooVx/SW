3acd69e252ca44e3693aaf75a919b5dd8469203a
student <student@Lab-112.ia.agh.edu.pl>
2017-03-14 10:44:13 +0100

    lab3 - 1.0

--- lab3/lab3_1.m
+++ lab3/lab3_1.m
@@ -1,2 +1,29 @@
+jet=imread('jet.bmp');
 
+figure(1);
+LUT(jet,kwadratowa)
+title('kwadratowa')
 
+figure(2);
+LUT(jet,sq)
+title('sq')
+
+figure(3);
+LUT(jet,sqr)
+title('sqr')
+
+figure(4);
+LUT(jet,log)
+title('log')
+
+figure(5);
+LUT(jet,odwlog)
+title('odwlog')
+
+figure(6);
+LUT(jet,odwrotna)
+title('odrwotna')
+
+figure(7);
+LUT(jet,pila)
+title('pila')
--- lab3/lab3_2.m
+++ lab3/lab3_2.m
@@ -1,1 +1,116 @@
 
+clear all;
+close all;
+
+jet=imread('jet.bmp');
+lena=imread('lena.bmp');
+%%%%%%%%%%%  add
+add=imadd(jet,lena,'uint16');
+figure(1);
+subplot(3,1,1)
+imshow(jet);
+title('jet')
+subplot(3,1,2)
+imshow(lena);
+title('lena')
+subplot(3,1,3)
+imshow(add,[]);
+title('add')
+
+%%%%%%%%%%%%%%% lin
+figure(2);
+subplot(2,3,1)
+imshow(jet)
+title('orginal')
+subplot(2,3,4)
+imshow(imlincomb(2,jet))
+title('zmodyfikowany')
+
+subplot(2,3,2)
+imshow(jet)
+title('orginal')
+subplot(2,3,5)
+imshow(imlincomb(6,jet))
+title('zmodyfikowany')
+
+
+subplot(2,3,3)
+imshow(jet)
+title('orginal')
+subplot(2,3,6)
+imshow(imlincomb(1,jet))
+title('zmodyfikowany')
+
+%%%%%%%%% add2
+add=imadd(jet,lena,'uint16');
+figure(3);
+subplot(3,1,1)
+imshow(jet);
+title('jet')
+subplot(3,1,2)
+imshow(lena);
+title('lena')
+subplot(3,1,3)
+imshow(add,[]);
+title('add')
+
+%%%%%%%%%%%% odj
+
+ lena16=int16(lena);
+ jet16=int16(jet);
+
+odj=imsubtract(lena16,jet16);
+
+figure(4);
+subplot(3,1,1)
+imshow(jet16,[]);
+title('jet')
+subplot(3,1,2)
+imshow(lena16,[]);
+title('lena')
+subplot(3,1,3)
+imshow(odj,[]);
+title('odj')
+
+figure(5);
+subplot(3,1,1)
+imshow(jet16,[]);
+title('jet')
+subplot(3,1,2)
+imshow(lena16,[]);
+title('lena')
+subplot(3,1,3)
+imshow(imabsdiff(lena16,jet16),[]);
+title('odj')
+
+
+%%%%%%%%%%%% mnozenie
+
+
+mn=immultiply(lena16,jet16);
+
+figure(6);
+subplot(3,3,1:3)
+imshow(jet16)
+title('jet')
+
+subplot(3,3,4:6)
+imshow(lena16)
+title('lena')
+
+subplot(3,3,7)
+imshow(jet)
+title('orginal')
+subplot(3,3,5)
+imshow(imlincomb(6,jet))
+title('lena x jet')
+
+
+subplot(3,3,3)
+imshow(jet)
+title('orginal')
+subplot(3,3,6)
+imshow(imlincomb(1,jet))
+title('zmodyfikowany')
+
+
