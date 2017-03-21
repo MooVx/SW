90bfde364c2d15b050c6c7d0845c39672ed1b820
MooVx <michal.tajak@gmail.com>
2017-03-21 14:31:25 +0100

    home

--- lab2/lab2_4.m
+++ lab2/lab2_4.m
@@ -10,22 +10,7 @@ lena_45=imrotate(lena,45);
 lena__45=imrotate(lena,-45);
 
 % 
-% figure(1);
-% subplot(2,3,1);
-% imshow(lena,[]);
-% title('Oryginal');
-% subplot(2,3,2);
-% imshow(lena_9,[]);
-% title('90');
-% subplot(2,3,3);
-% imshow(lena_45,[]);
-% title('45');
-% subplot(2,3,4);
-% imshow(lena__45,[]);
-% title('-45');
-% subplot(2,3,5);
-% imshow(lena__9,[]);
-% title('-90');
+
 
 
 %wczytanie pliku do workspace
