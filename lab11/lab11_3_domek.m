56a9d8aa545023d3c87a124c30636fe01763a441
MooVx <michal.tajak@gmail.com>
2017-05-27 18:47:26 +0200

    domek

--- lab11/lab11_3_domek.m
+++ lab11/lab11_3_domek.m
@@ -3,76 +3,27 @@ clear all
 
 load maskiPP;
 
-img = imread('lab112.bmp');
+img = imread('dom.tif');
 
+ img=imsharpen(img);
+ img = medfilt2(img);
 imshow(img,[]);
 
-[X,Y,Z] = size(img);
-labeled(X,Y)=0;
-TH = 60;
+BW = edge(img,'Prewitt','nothinning');
+figure(1)
+imshow(BW,[]);
 
-for i=1:X
-    for j=1:Y
-        
-        if img(i,j,1)<TH && img(i,j,2)<TH && img(i,j,3)<TH
-             labeled(i,j) = 1;
-        else
-            labeled(i,j) = 0;
-        end
-        
-    end
-end
-
-
-
-M =[1 2 1;
-    2 4 2; 
-    1 2 1];
-M = M/sum(sum(M));
-filtered = conv2(labeled,M,'same');
-
-filtered = bwlabel(filtered,8);
-
-r = regionprops(filtered,'Centroid');
+r = regionprops(BW,'Centroid');
 for i=1:length(r)
     text(r(i).Centroid(1),r(i).Centroid(2),['\color{magenta}',num2str(i)]);
 end
-
-
-wspolczynniki = obliczWspolczynniki(filtered);
-[X,Y] = size(filtered);
-wydzielone(X,Y)=0;
-
-for i=1:X
-    for j=1:Y
-        
-        piksel = filtered(i,j);
-        if piksel ~= 0 && (wspolczynniki(piksel,2) > 0.74 && wspolczynniki(piksel,2) < 0.8)
-
-             
-            wydzielone(i,j) = 1;
-        end
-        
-    end
-end
-
-imshow(wydzielone,[]);
-
-img=wydzielone;
-
-load maskiPP;
-
-img_s1=conv2(double(img),S1,'same');
-img_s2=conv2(double(img),S2,'same');
-BW=(abs(img_s1) +abs(img_s2));
-
-imshow(BW,[]);
+figure(2)
 [H,T,R] = hough(BW);
 imshow(H,[],'XData',T,'YData',R,...
             'InitialMagnification','fit');
 xlabel('\theta'), ylabel('\rho');
 axis on, axis normal, hold on;
-P  = houghpeaks(H,8,'threshold',ceil(0.3*max(H(:))));
+P  = houghpeaks(H,40,'threshold',ceil(0.3*max(H(:))));
 x = T(P(:,2)); y = R(P(:,1));
 plot(x,y,'s','color','white');
 % Find lines and plot them
